d984fce660edd392108640968b9d4a524a4f6a5d
Moin Uddin <U_Moin75@yahoo.com>
2019-05-15 02:06:42 -0400

    Add JWTDecode and iOS API function calls for authentication

---
+++ TeleSwap/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  TeleSwap
+//
+//  Created by Moin Uddin on 5/15/19.
+//  Copyright © 2019 Cameron Dunn. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case post = "POST"
+    case put = "PUT"
+    case get = "GET"
+    case delete = "DELETE"
+}
---
+++ TeleSwap/UserDefaultKeys.swift
@@ -0,0 +1,15 @@
+//
+//  UserDefaultKeys.swift
+//  TeleSwap
+//
+//  Created by Moin Uddin on 5/15/19.
+//  Copyright © 2019 Cameron Dunn. All rights reserved.
+//
+
+import Foundation
+
+
+enum UserDefaultsKeys: String {
+    case userId
+    case token
+}
