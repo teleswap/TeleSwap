25f5ee9290ca18a05c34fde659abcb0f32820adc
Cameron <Cameron.dunn2013@gmail.com>
2019-05-18 15:21:58 -0500

    -Assign delegates and data sources for contents of new post screen

--- TeleSwap/View Controllers/PostViewController.swift
+++ TeleSwap/View Controllers/PostViewController.swift
@@ -9,7 +9,15 @@
 import Foundation
 import UIKit
 
-class PostViewController : UIViewController{
+class PostViewController : UIViewController, UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
